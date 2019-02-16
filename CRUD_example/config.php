d63b05f68a7ecbe7b7d926cacf10ecf108f5e222
rifqi31 <muttaqinrifqi31@gmail.com>
2019-02-16 14:52:09 +0700

    add crud example

---
+++ CRUD_example/config.php
@@ -0,0 +1,16 @@
+<?php
+// identify server
+$server = "localhost";
+$user = "root";
+$password = "pedhfynbn";
+$db_name = "pendaftaran_siswa";
+
+$db = mysqli_connect($server, $user, $password, $db_name);
+
+if( !$db ) {
+    die("Gagal terhubung ke database". mysqli_connect_error());
+} else {
+    echo "db has been connected";
+}
+
+?>
