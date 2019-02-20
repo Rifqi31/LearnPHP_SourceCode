5ebfab1ccdd39113da2d3c6261b335816140d68a
rifqi31 <muttaqinrifqi31@gmail.com>
2019-02-20 08:31:53 +0700

    example strings in php

---
+++ basic/strings.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Strings in PHP</title>
+</head>
+<body>
+    <?php
+        // example function strlen()
+        echo strlen("Hello World");
+    ?>
+</body>
+</html>
