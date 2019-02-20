ece15510acd04856d071a15130894f27a2091791
rifqi31 <muttaqinrifqi31@gmail.com>
2019-02-20 10:22:07 +0700

    example of different aposthrope and quote

---
+++ basic/var_test.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Var test</title>
+</head>
+<body>
+    <?php
+
+        // different apostrhope and quote
+        // make variable
+        $vardata1 = 31011996;
+
+        // test it
+        echo 'this is variable $vardata1';
+        echo "<br>";
+        echo "this is variable $vardata1";
+
+    ?>
+</body>
+</html>
