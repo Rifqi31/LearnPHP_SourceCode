8a776fd19f41eb6fe942dc5bf7914f75cb65415c
rifqi31 <muttaqinrifqi31@gmail.com>
2019-02-20 08:41:54 +0700

    add some string functions

--- basic/strings.php
+++ basic/strings.php
@@ -9,7 +9,32 @@
 <body>
     <?php
         // example function strlen()
+        // length of strings
         echo strlen("Hello World");
+
+        echo "<br>";
+
+        // example function str_word_count()
+        // count how many words in strings
+        echo str_word_count("Hello World");
+    
+        echo "<br>";
+
+        // example function strrev()
+        // revers strings
+        echo strrev("Hello World");
+
+        echo "<br>";
+
+        // example function for strpos()
+        // find specific strings
+        echo strpos("Hello World", "Hello");
+
+        echo "<br>";
+
+        // example function for str_replace()
+        // replace specific strings
+        echo str_replace("world", "Natasha Teen", "Hello world");
     ?>
 </body>
 </html>
