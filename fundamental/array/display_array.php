785ae152a2677e8ec9d3cec1c113729cf0247865
rifqi31 <muttaqinrifqi31@gmail.com>
2019-02-21 05:31:29 +0700

    just upload

---
+++ fundamental/array/display_array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Displaying Array</title>
+</head>
+<body>
+    <?php
+        // diising dengan menunjuk nomor indeksnya
+        $car[0] = "VW";
+        $car[1] = "Ferrari";
+        $car[2] = "Daihatsu";
+        $car[3] = "Honda";
+        $car[4] = "Mitsubishi";
+        $car[5] = "Toyota";
+        $car[6] = "Yamaha"; 
+
+    ?>
+</body>
+</html>
