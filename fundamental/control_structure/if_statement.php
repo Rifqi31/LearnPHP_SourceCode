440b63faecb4f6ff57f925041f9c0621841f03b4
rifqi31 <muttaqinrifqi31@gmail.com>
2019-02-21 17:43:34 +0700

    add example if statement

---
+++ fundamental/control_structure/if_statement.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>If Statement</title>
+</head>
+<body>
+    <h3>Contoh If Statement</h3>
+    <pre>
+        if (ekspresi) {
+            perintah 1;
+            perintah 2;
+            perintah 3;
+        }
+    </pre>
+    <?php
+        $varnum1 = 5;
+        $varnum2 = 7;
+        if ($varnum1 < $varnum2) {
+            echo "$varnum2 is greater than $varnum1";
+        }
+    ?>
+</body>
+</html>
