3cddca0c9c458def080239c520e44e4fa922e8a8
rifqi31 <muttaqinrifqi31@gmail.com>
2019-02-21 08:32:01 +0700

    example how to disply array data type

--- fundamental/array/display_array.php
+++ fundamental/array/display_array.php
@@ -9,14 +9,19 @@
 <body>
     <?php
         // diising dengan menunjuk nomor indeksnya
-        $car[0] = "VW";
-        $car[1] = "Ferrari";
-        $car[2] = "Daihatsu";
-        $car[3] = "Honda";
-        $car[4] = "Mitsubishi";
-        $car[5] = "Toyota";
-        $car[6] = "Yamaha"; 
-
-    ?>
+        $car[0] = "Daihatsu";
+        $car[1] = "Honda";
+        $car[2] = "Mitsubishi";
+        $car[3] = "Toyota";
+        $car[4] = "Yamaha"; 
+        
+        echo "isi dari array \$car[0] adalah $car[0] <br>";
+        echo "isi dari array \$car[1] adalah $car[1] <br>";
+        echo "isi dari array \$car[2] adalah $car[2] <br>";
+        echo "isi dari array \$car[3] adalah $car[3] <br>";
+        echo "isi dari array \$car[4] adalah $car[4] <br>";
+        
+        
+        ?>
 </body>
 </html>
