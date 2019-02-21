796b0c229714ddf2aece3a36fa9e729e6fc78965
rifqi31 <muttaqinrifqi31@gmail.com>
2019-02-21 09:22:10 +0700

    add function array_unique()

--- fundamental/array/array_union_merge.php
+++ fundamental/array/array_union_merge.php
@@ -9,19 +9,25 @@
 <body>
     <?php 
         echo "<h3> Contoh array_merge()</h3>";
-        $varnum1 = array(1,2,3,4);
-        $varnum2 = array("abc", 22, 90);
-
+        $varnum1 = array(1,2,3,4,22,90);
         echo "Array pertama : ";
         print_r($varnum1);
-
-        echo "<br>Array kedua : ";
+        
+        $varnum2 = array("abc", 22, 90);
+        echo "<br><br>Array kedua : ";
         print_r($varnum2);
 
+        
         $hasil = array_merge($varnum1, $varnum2);
-
-        echo "<br>setelah digabung: ";
+        echo "<br><br>setelah digabung: ";
         print_r($hasil);
+        
+
+        // contoh untuk array_unique jika ada value yang sama
+        $unique_result = array_unique($hasil);
+        echo "<br><br>setelah menggunakan array_unique() <br>";
+        print_r($unique_result);
+
     ?>
 </body>
 </html>
