99ec2380a92e1744cc8f0edf951dee0b04c6af43
rifqi31 <muttaqinrifqi31@gmail.com>
2019-02-22 07:53:45 +0700

    add for statement

---
+++ fundamental/control_structure/for_statement.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>For Statement</title>
+</head>
+<body>
+    <h3>For Statement</h3>
+
+    <?php
+        $varnum = array(33, 22, 21,-322,-133);
+        for ($counter=0; $counter <=4; $counter++) { 
+            if ($varnum[$counter] % 2 == 0) {
+                echo $varnum[$counter]." = bilangan genap <br>";
+            } else {
+                echo $varnum[$counter]." = bilangan ganjil <br>";
+            }
+        }
+
+    ?>
+</body>
+</html>
